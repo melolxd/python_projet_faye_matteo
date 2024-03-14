56a57e2ebdeaa598ad0967ef6c40299b0576e366
melolxd <matteo.faye42@gmail.com>
2024-03-14 15:23:02 +0100

    commit discover, page de découverte

--- code.py
+++ code.py
@@ -1,36 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-# Chemin vers la page de login (page principale)
 @app.route('/')
 def login():
-    # Code pour la page de login (pas encore implémenté)
     return render_template('login.html')
 
-# Chemin vers la page principale
 @app.route('/index')
 def index():
-    # Code pour la page principale avec la barre latérale gauche et les mini-jeux (pas encore implémenté)
-    return render_template('left_menu.html')
+    return render_template('index.html')
 
-# Chemin vers le jeu "Trouver joueurs avec image"
-@app.route('/jeu/trouver-joueurs-avec-image')
-def jeu_trouver_joueurs_avec_image():
-    # Code pour le jeu "Trouver joueurs avec image" (pas encore implémenté)
-    return "Jeu Trouver joueurs avec image"
-
-# Chemin vers le jeu "Parcours de club"
-@app.route('/jeu/parcours-de-club')
-def jeu_parcours_de_club():
-    # Code pour le jeu "Parcours de club" (pas encore implémenté)
-    return "Jeu Parcours de club"
-
-# Chemin vers le jeu "Composition équipe"
-@app.route('/jeu/composition-equipe')
-def jeu_composition_equipe():
-    # Code pour le jeu "Composition équipe" (pas encore implémenté)
-    return "Jeu Composition équipe"
+@app.route('/discover')
+def discover():
+    return render_template('discover.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
