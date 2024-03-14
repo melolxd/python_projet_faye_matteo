79cd64fbda2627877291c6d295ca6278cc7b2426
melolxd <matteo.faye42@gmail.com>
2024-03-14 15:55:59 +0100

    connection et register

--- code.py
+++ code.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
+def racine():
+    return render_template('racine.html')
+
+@app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 @app.route('/index')
 def index():
